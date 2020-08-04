9eae76250dd50028dd50adc3578bd9ea790ace92
haleykelling <haley@kellings.com>
2020-08-04 11:51:57 -0600

    Create resources for trip and day. Create relationship for trip having many days. Create seeds for two trips and many days.

---
+++ db/migrate/20200804172817_create_days.rb
@@ -0,0 +1,13 @@
+class CreateDays < ActiveRecord::Migration[6.0]
+  def change
+    create_table :days do |t|
+      t.integer :number
+      t.date :date
+      t.string :start_city
+      t.string :end_city
+      t.references :trip, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
