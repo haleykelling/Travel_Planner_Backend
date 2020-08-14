121d624a76e18f5e77c60baa40b6df0a0e0a73c9
haleykelling <haley@kellings.com>
2020-08-14 15:37:36 -0600

    Debugging with heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,16 @@ class ApplicationController < ActionController::API
         header = request.headers['Authorization']
         token = header.split(" ")[1]
         render json: {secret: secret}
-        if !token 
-            render json: {error: "Must be logged in"}, status: :forbidden
-        else
-            begin
-                payload = JWT.decode(token, secret)[0]
-                @user = User.find(payload['user_id'])
-            rescue
-                render json: { error: "Must be logged in" }, status: :forbidden 
-            end
-        end
+        # if !token 
+        #     render json: {error: "Must be logged in"}, status: :forbidden
+        # else
+        #     begin
+        #         payload = JWT.decode(token, secret)[0]
+        #         @user = User.find(payload['user_id'])
+        #     rescue
+        #         render json: { error: "Must be logged in" }, status: :forbidden 
+        #     end
+        # end
     end
 
 end
